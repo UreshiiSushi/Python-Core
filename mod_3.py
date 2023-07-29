8fd95f59725bb7af8e752043fd99f883c0c52a84
UreshiiSushi <merieru@gmail.com>
2023-07-29 13:35:57 +0200

    New file for module 3

---
+++ mod_3.py
@@ -0,0 +1,69 @@
+#region Ex.1
+# 
+# def greeting():
+    # print("Hello world!")
+# 
+# greeting()
+#endregion
+
+#region Ex. 2
+# def invite_to_event(username):
+#     return f"Dear {username}, we have the honour to invite you to our event."
+
+# name = input("Enter your name: ")
+# print(invite_to_event(name))
+#endregion
+
+#region Ex. 3
+# base_rate = 40
+# price_per_km = 10
+# total_trip = 0
+# 
+# 
+# def calculate_trip_price(distance_km):
+    # global total_trip 
+    # total_trip += 1
+    # return base_rate + price_per_km*distance_km
+# 
+# distance = float(input("Enter a distance in km: "))
+# print(calculate_trip_price(distance))
+# print(total_trip)
+    # 
+#endregion
+
+#region Ex. 4
+# def discount_price(price, discount):
+#     def apply_discount():
+#         nonlocal price
+#         price = price*(1 - discount)
+#     apply_discount()
+#     return price
+#endregion
+
+#region Ex. 5
+# def get_fullname(first_name, last_name, middle_name = ''):
+#     if middle_name:
+#         return f"{first_name} {middle_name} {last_name}"
+#     else:
+#         return f"{first_name} {last_name}"
+#endregion
+
+#region Ex. 6
+
+#endregion
+
+#region Ex. 7
+
+#endregion
+#region Ex. 8
+
+#endregion
+#region Ex. 9
+
+#endregion
+#region Ex. 10
+
+#endregion
+#region Ex. 11
+
+#endregion 
