89e31a5657f9fcdff18b731731ff8a72d806a2f4
UreshiiSushi <merieru@gmail.com>
2023-07-31 00:56:44 +0200

    Finish modul 3

--- mod_3.py
+++ mod_3.py
@@ -49,21 +49,75 @@
 #endregion
 
 #region Ex. 6
-
+# def format_string(string, length):
+#     if len(string) >= length:
+#         return string
+#     else:
+#         string = " "*((length - len(string))//2) + string
+#         return string
+    
+# in_string = input("String: ")
+# in_len = int(input("Length: "))
+# print(format_string(in_string, in_len))
 #endregion
 
 #region Ex. 7
-
+# def first(size, *position):
+#     return size + len(position)
+# def second(size, **keys):
+#     return size + len(keys)
+# print(first(5, "first", "second", "third"))
+# print(first(1, "Alex", "Boris"))
+# print(second(3, comment_one="first", comment_two="second", comment_third="third"))
+# print(second(10, comment_one="Alex", comment_two="Boris"))
 #endregion
+
 #region Ex. 8
+# def cost_delivery(quantity, *_, discount = 0):
+#     if not discount:
+#         return 5 + 2*(quantity-1)
+#     else:
+#         return (5 + 2*(quantity - 1))*(1 - discount)
 
+# print(cost_delivery(2, 1, 2, 3))
+# print(cost_delivery(3, 3))
+# print(cost_delivery(1))
+# print(cost_delivery(2, 1, 2, 3, discount=0.5))
 #endregion
+
 #region Ex. 9
+# def cost_delivery(quantity, *_, discount=0):
+#     """Функція повертає суму за доставлення замовлення.
 
+#      Перший параметр &mdash; кількість товарів в замовленні.
+#      Параметр знижки discount, який передається лише як ключовий, за замовчуванням має значення 0."""
+#     result = (5 + 2 * (quantity - 1)) * (1 - discount)
+#     return result
+
+# print(cost_delivery.__doc__)
 #endregion
+
 #region Ex. 10
+# def factorial(n):
+#     if n < 2:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
 
+# def number_of_groups(n, k):
+#     return int(factorial(n)/(factorial(n-k)*factorial(k)))
+
+# print(number_of_groups(50, 7))
 #endregion
-#region Ex. 11
 
+#region Ex. 11
+def fibonacci(n):
+    if n <= 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n - 2)
+
+print(fibonacci(10))
 #endregion 
