4a574be1f34112dc6ee4d13efd038e75fbed4515
UreshiiSushi <merieru@gmail.com>
2023-07-31 00:58:50 +0200

    Added file for module 4

---
+++ mod_4.py
@@ -0,0 +1,42 @@
+#region Ex. 1
+
+#endregion
+#region Ex. 2
+
+#endregion
+#region Ex. 3
+
+#endregion
+#region Ex. 4
+
+#endregion
+#region Ex. 5
+
+#endregion
+#region Ex. 6
+
+#endregion
+#region Ex. 7
+
+#endregion
+#region Ex. 8
+
+#endregion
+#region Ex. 9
+
+#endregion
+#region Ex. 10
+
+#endregion
+#region Ex. 11
+
+#endregion
+#region Ex. 12
+
+#endregion
+#region Ex. 13
+
+#endregion
+#region Ex. 14
+
+#endregion
