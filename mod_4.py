c9c39ca0d0168b0f7660ddfe1ff983e8438c2276
UreshiiSushi <merieru@gmail.com>
2023-08-03 21:34:44 +0200

    Add first two exercise

--- mod_4.py
+++ mod_4.py
@@ -1,12 +1,27 @@
 #region Ex. 1
-
+# def amount_payment(payment):
+#     sum_pay = 0
+#     for i in payment:
+#         if i > 0:
+#             sum_pay += i
+#     return sum_pay
 #endregion
+
 #region Ex. 2
-
+# def prepare_data(data):
+#     data.sort()
+#     data.pop()
+#     data.pop(0)
+#     return data
+    
+# test_list = [1, -3, 4, 100, 0, -5, 10, 1, 1]
+# print(prepare_data(test_list))
 #endregion
+
 #region Ex. 3
 
 #endregion
+
 #region Ex. 4
 
 #endregion
